e770cb2f9a06e80c02d34849addf42a0f892c3a3
Rocco <roccolavecchia.rl@gmail.com>
2025-05-26 11:19:33 -0300

    arregle el boton

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,7 +1,5 @@
-"use client"
 
 import { useState, useEffect } from "react"
-import { Search, X } from "lucide-react"
 
 export default function SearchBar({ setSearch }) {
   const [inputValue, setInputValue] = useState("")
@@ -13,28 +11,16 @@ export default function SearchBar({ setSearch }) {
     return () => clearTimeout(timer)
   }, [inputValue, setSearch])
 
-  const clearSearch = () => {
-    setInputValue("")
-    setSearch("")
-  }
+
 
   return (
-    <div className="search-container">
-      <div className="search-icon">
-        <Search size={16} className="icon" />
-      </div>
+   <div className="search-container">
       <input
-        type="text"
+        type="search"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
         placeholder="Buscar Canciones..."
       />
-
-      {inputValue && (
-        <button onClick={clearSearch} className="clear-button" aria-label="Limpiar búsqueda">
-          <X size={16} className="icon" />
-        </button>
-      )}
     </div>
   )
 }
